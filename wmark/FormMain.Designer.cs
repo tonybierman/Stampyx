0832e03def55cb9b585fc3e89dd11f494bb4b180
tonybierman <tonybierman@gmail.com>
2018-05-05 10:16:36 -0500

    Initial code commit

---
+++ wmark/FormMain.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace wmark
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWatermark = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPrefix = new System.Windows.Forms.TextBox();
+            this.chkboxMaintMode = new System.Windows.Forms.CheckBox();
+            this.textBoxBody = new System.Windows.Forms.TextBox();
+            this.btnRepeat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnWatermark
+            // 
+            this.btnWatermark.Location = new System.Drawing.Point(68, 12);
+            this.btnWatermark.Name = "btnWatermark";
+            this.btnWatermark.Size = new System.Drawing.Size(111, 23);
+            this.btnWatermark.TabIndex = 0;
+            this.btnWatermark.Text = "Select source folder";
+            this.btnWatermark.UseVisualStyleBackColor = true;
+            this.btnWatermark.Click += new System.EventHandler(this.btnWatermark_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(65, 131);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Click += new System.EventHandler(this.lblStatus_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Prefix new images with";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxPrefix
+            // 
+            this.textBoxPrefix.Location = new System.Drawing.Point(177, 47);
+            this.textBoxPrefix.Name = "textBoxPrefix";
+            this.textBoxPrefix.Size = new System.Drawing.Size(53, 20);
+            this.textBoxPrefix.TabIndex = 3;
+            this.textBoxPrefix.Text = "wm_";
+            this.textBoxPrefix.TextChanged += new System.EventHandler(this.textBoxPrefix_TextChanged);
+            // 
+            // chkboxMaintMode
+            // 
+            this.chkboxMaintMode.AutoSize = true;
+            this.chkboxMaintMode.Checked = true;
+            this.chkboxMaintMode.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkboxMaintMode.Location = new System.Drawing.Point(68, 73);
+            this.chkboxMaintMode.Name = "chkboxMaintMode";
+            this.chkboxMaintMode.Size = new System.Drawing.Size(148, 17);
+            this.chkboxMaintMode.TabIndex = 4;
+            this.chkboxMaintMode.Text = "Folder maintenance mode";
+            this.chkboxMaintMode.UseVisualStyleBackColor = true;
+            // 
+            // textBoxBody
+            // 
+            this.textBoxBody.Location = new System.Drawing.Point(68, 97);
+            this.textBoxBody.Name = "textBoxBody";
+            this.textBoxBody.Size = new System.Drawing.Size(162, 20);
+            this.textBoxBody.TabIndex = 5;
+            this.textBoxBody.Text = "© Tony Bierman";
+            this.textBoxBody.TextChanged += new System.EventHandler(this.textBoxBody_TextChanged);
+            // 
+            // btnRepeat
+            // 
+            this.btnRepeat.Location = new System.Drawing.Point(185, 12);
+            this.btnRepeat.Name = "btnRepeat";
+            this.btnRepeat.Size = new System.Drawing.Size(45, 23);
+            this.btnRepeat.TabIndex = 6;
+            this.btnRepeat.Text = "...";
+            this.btnRepeat.UseVisualStyleBackColor = true;
+            this.btnRepeat.Click += new System.EventHandler(this.btnRepeat_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 164);
+            this.Controls.Add(this.btnRepeat);
+            this.Controls.Add(this.textBoxBody);
+            this.Controls.Add(this.chkboxMaintMode);
+            this.Controls.Add(this.textBoxPrefix);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnWatermark);
+            this.Name = "FormMain";
+            this.Text = "Watermarker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWatermark;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPrefix;
+        private System.Windows.Forms.CheckBox chkboxMaintMode;
+        private System.Windows.Forms.TextBox textBoxBody;
+        private System.Windows.Forms.Button btnRepeat;
+    }
+}
+
---
+++ wmark/FormMain.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wmark
+{
+    public partial class FormMain : Form
+    {
+        string m_path = string.Empty;
+        readonly string PATH_DEST = @"E:\usr\var\media\Watermarked";
+        readonly string WMARK_FONT_FAMILY = "Georgia";
+        readonly int WMARK_FONT_SIZE = 64;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            // Disallow creation of new files using the FolderBrowserDialog.
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            this.folderBrowserDialog1.Description = "Select the source images directory";
+        }
+
+        private void process(string path)
+        {
+            lblStatus.Text = String.Empty;
+            Image img = null;
+            string pathSource = String.Empty;
+            string pathTarget = String.Empty;
+
+            try
+            {
+                if (!Directory.Exists(PATH_DEST))
+                    throw new FileNotFoundException(PATH_DEST);
+
+                string[] imgExtensions = { "*.jpg" };
+                List<FileInfo> files = new List<FileInfo>();
+                DirectoryInfo dir = new DirectoryInfo(path);
+                int c = 0;
+                foreach (string e in imgExtensions)
+                {
+                    FileInfo[] folder = dir.GetFiles(e, SearchOption.AllDirectories);
+                    foreach (FileInfo file in folder)
+                    {
+                        c++;
+                        lblStatus.Text = string.Format("Processing {0}", c);
+                        lblStatus.Refresh();
+                        using (FileStream fs = file.OpenRead())
+                        {
+                            pathSource = string.Concat(path, @"\", file.Name);
+
+                            // Skip watermarks images created by this app
+                            if (file.Name.StartsWith(textBoxPrefix.Text))
+                                continue;
+
+                            // Path of the watermarked image to be created
+                            pathTarget = string.Concat(PATH_DEST, @"\", textBoxPrefix.Text, file.Name);
+                            bool targetExists = File.Exists(pathTarget);
+
+                            // If maintenance mode and watermarked image already exists,
+                            // then skip it.  Otherwise, delete if it exists.
+                            if (targetExists && chkboxMaintMode.Checked)
+                                continue;
+                            else if (targetExists)
+                                File.Delete(pathTarget);
+
+                            // Create the watermarked image
+                            Stream outputStream = new MemoryStream();
+                            AddWatermark(fs, textBoxBody.Text, outputStream);
+                            img = Image.FromStream(outputStream);
+                            using (Bitmap savingImage = new Bitmap(img.Width, img.Height, img.PixelFormat))
+                            {
+                                savingImage.SetResolution(img.HorizontalResolution, img.VerticalResolution);
+                                ImageCodecInfo codecInfo = GetEncoder(ImageFormat.Jpeg);
+
+                                // Create an Encoder object based on the GUID  
+                                // for the Quality parameter category.  
+                                System.Drawing.Imaging.Encoder myEncoder =
+                                    System.Drawing.Imaging.Encoder.Quality;
+
+                                // Create an EncoderParameters object.  
+                                // An EncoderParameters object has an array of EncoderParameter  
+                                // objects. In this case, there is only one  
+                                // EncoderParameter object in the array.  
+                                EncoderParameters parms = new EncoderParameters(1);
+
+                                EncoderParameter parm = new EncoderParameter(myEncoder, 100L);
+                                parms.Param[0] = parm;
+
+                                using (Graphics g = Graphics.FromImage(savingImage))
+                                    g.DrawImage(img, new Point(0, 0));
+
+                                savingImage.Save(pathTarget, codecInfo, parms);
+                            }
+                        }
+
+                        img.Dispose();
+
+                        // This would would delete the original
+                        // file.Delete(); 
+                    }
+                }
+
+                lblStatus.Text = "Processing Completed";
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "An error occured";
+                MessageBox.Show(ex.ToString(), "An error occured");
+            }
+            finally
+            {
+                if (img != null)
+                    img.Dispose();
+            }
+        }
+
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+        private void AddWatermark(FileStream fs, string watermarkText, Stream outputStream)
+        {
+            Image img = Image.FromStream(fs);
+            Font font = new Font(WMARK_FONT_FAMILY, WMARK_FONT_SIZE, FontStyle.Regular, GraphicsUnit.Pixel);
+
+            //Adds a transparent watermark with an 100 alpha value.
+            Color color = Color.FromArgb(100, 255, 255, 255);
+
+            //The position where to draw the watermark on the image
+            Point pt = new Point(40, img.Height - 120);
+            SolidBrush sbrush = new SolidBrush(color);
+
+            Graphics gr = null;
+            try
+            {
+                gr = Graphics.FromImage(img);
+                gr.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height));
+                //gr.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            }
+            catch (Exception ex)
+            {
+                // http://support.microsoft.com/Default.aspx?id=814675
+                Image img1 = img;
+                img = new Bitmap(img1, img.Width, img.Height);
+                gr = Graphics.FromImage(img);
+                gr.DrawImage(img1, new Rectangle(0, 0, img.Width, img.Height));//, 0, 0, img.Width, img.Height, GraphicsUnit.Pixel);
+                img1.Dispose();
+
+                lblStatus.Text = ex.Message;
+            }
+
+            gr.DrawString(watermarkText, font, sbrush, pt);
+            gr.Dispose();
+
+            // Save to memory stream
+            
+
+            img.Save(outputStream, ImageFormat.Jpeg);
+        }
+
+        #region Form events
+
+        private void btnWatermark_Click(object sender, EventArgs e)
+        {
+            string path = String.Empty;
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                path = folderBrowserDialog1.SelectedPath;
+            }
+
+            if (path == String.Empty)
+            {
+                lblStatus.Text = "Invalid directory..";
+                return;
+            }
+
+            m_path = path;
+
+            process(path);
+        }
+
+        private void lblStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxBody_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRepeat_Click(object sender, EventArgs e)
+        {
+            process(m_path);
+        }
+
+        #endregion Form events 
+
+        private void textBoxPrefix_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
